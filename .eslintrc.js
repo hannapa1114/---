475c6fa1a38a904958657910de15f82c88e5022d
{krism01} <{krism01@naver.com}>
2020-09-23 14:30:43 +0900

    feature/basic_server test
    server 기본틀 생성: #11

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: [
+    "eslint:recommended", // 미리 설정된 규칙 세트을 사용한다
+  ],
+}
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// controllers/index.js 안에 usersController 를 사용
+const { usersController } = require('../controller');
+
+// * POST /user/signin
+router.post('/signin', usersController.signin.post);
 
 module.exports = router;
