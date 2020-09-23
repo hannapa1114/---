d425e1e50f60df5fbc8a15aa34eb12f075f4c76f
{krism01} <{krism01@naver.com}>
2020-09-24 08:48:59 +0900

    feature/basic_server
    fix : # 11

--- server/index.js
+++ server/index.js
@@ -1,53 +1,45 @@
 //declare Express
 const express = require('express');
 
-//express와 mysql 연결(1) // ???
-const sequelize = require('./models/index').sequelize;
-
 //middleware
-const bodyparser = require('body -parser');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 const session = require('express-session');
 const cors = require('cors');
-const cookieparser = require('cookie-parser');
-const morgan = require('morgan')
 
 //Routes
-const userRouter = require('./routes/user')
-const dessertRouter = require('./routes/dessert')
-const commentRouter = require('./routes/comment')
-const searchRouter = require('./routes/search')
+const usersRouter = require('./routes/users');
 
-const app = express()
-// const port = 4000;
-
-//express와 mysql 연결()
-sequelize.sync()
+const app = express();
+const port = 4000;
 
 //use middleware
-app.use(bodyparser.json());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:3000'],
-    // method: ['GET', 'POST'],
-    credentials: true
+  origin: ['http://localhost:3000'],
+  method: ['GET', 'POST'],
+  credentials: true
 }))
-app.use(morgan('dev'))
 app.use(session({ // 세션을 관리하기 위해 필요한 미들웨어.
-    secret : 'Secret Key',
-    resave: true,
-    saveUninitialized: true
+  secret : 'Fantastic Four',
+  resave: true,
+  saveUninitialized: true
 }))
-app.use(cookieparser()) //요청된 쿠키를 쉽게 추출할 수 있도록 해주는 미들웨어.
 
 
 app.get('/', (req,res) => {
-    res.status(200).send('Success!')
+  res.status(200).send('Success!')
 })
 
-app.use('/user', userRouter)
-app.use('/dessert', dessertRouter)
-app.use('/comment', commentRouter)
-app.use('/search', searchRouter)
+// Router
+app.use('/users', usersRouter);
 
-app.listen(4000,() => {
-    console.log('데이터베이스 연결 성공');
+
+app.listen(port, () => {
+  console.log("connected", port);
 })
+
+module.exports = app;
