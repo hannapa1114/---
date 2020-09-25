f23b2c2503523548db6ad5a31592a6f1feb1e7e6
zi <sjm891114@gmail.com>
2020-09-25 09:46:41 +0900

    [Server] feature/signin

--- server/controller/users/signin.js
+++ server/controller/users/signin.js
@@ -1,7 +1,28 @@
-const { users } = require('../../models');
+const { user } = require("../../models");
 
 module.exports = {
   post: (req, res) => {
+    const { email, password } = req.body;
+    const sess = req.session;
 
-  }
-}
+    user
+      .findOne({
+        where: {
+          email: email,
+          password: password,
+        },
+      })
+      .then((data) => {
+        if (!data) {
+          return res.status(404).send("unvalid user");
+        }
+        sess.userid = data.id;
+        res.status(200).json({
+          id: data.id,
+        });
+      })
+      .catch((err) => {
+        res.status(404).send(err);
+      });
+  },
+};
